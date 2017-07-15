40e37d1d2c478a1ac7507eacda9eb35a9131e5dd
madhuvkiran777@gmail.com <madhuvkiran777@gmail.com>
2017-07-15 21:01:38 +0530

    bb php

--- receipt.php
+++ receipt.php
@@ -1,16 +1,10 @@
 <?php
 require 'DB_Connect.php';
-$name=$_POST["Name"];
-$email=$_POST["Email"];
-$password=md5($_POST["Password"]);
-$age=$_POST["Age"];
-$bgroup=$_POST["Bgroup"];
-$phone=$_POST["Mobile"];
-$gender=$_POST["Gender"];
-$city=$_POST["City"];
-$area=$_POST["Area"];
-$Weight=$_POST["Weight"];
-$query="INSERT INTO donor(Name,Email,Password,Age,Bgroup,Mobile,Gender,City,Area,Weight)VALUES('$name','$email','$password','$age','$bgroup','$phone','$gender','$city','$area','$Weight')";
+$name=$_POST["UserName"];
+$email=$_POST["UserEmail"];
+$password=md5($_POST["UserPassword"]);
+
+$query="INSERT INTO rec_reg(UserName,UserEmail,UserPassword)VALUES('$name','$email','$password')";
 $result=mysqli_query($conn, $query) or die("Error" .mysqli_error($conn));
 
 
