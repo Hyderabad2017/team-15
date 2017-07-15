26e50bf968c22fbdba0955082469ccb4e859ea10
SreePriya-A <sreepriya3996@gmail.com>
2017-07-16 02:58:54 +0530

    Add files via upload

---
+++ donationdetails.php
@@ -0,0 +1,88 @@
+<html>
+<head>
+<title>home</title>
+</head>
+<body>
+<h1>donation details </h1>
+<fieldset style="width:30%"><legend>Donation Form</legend>
+<form method="POST" action="date.php">
+<table border="0">
+<tr>
+<td>Username</td>
+<td><input type="text" name="user" id="user"></td>
+</tr>
+<tr>
+<td>DATE</td><td> <input type="date" name="dt" id="dt" onchange="mydate1();"></td>
+</tr>
+<tr>
+<td>BLOOD BANKS</td><td>  <select>
+  <option value="ABC" name="BB">bloodbank1</option>
+  <option value="VBN" name="BB">BLOODBANK2</option>
+</select>
+</td>
+</tr>
+<tr>
+<td>
+<input type="submit" name="submit" value="submit"></td></tr>
+</table>
+</form>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
